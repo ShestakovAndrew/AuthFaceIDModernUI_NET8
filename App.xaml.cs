e21beed94cc2e82d88a42c6d65c4e0a663df8342
Andrew <globalshow6@gmail.com>
2023-03-27 20:59:50 +0300

    Добавил окно логинации

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,6 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
-namespace WpfApp3
+namespace ModernLoginWindow
 {
     /// <summary>
     /// Interaction logic for App.xaml
---
+++ Windows/Login.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using AuthFaceIDModernUI.DataBase;
+
+namespace ModernLoginWindow
+{
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            using var db = new UsersContext();
+
+            if (!db.IsLoginExistInDB(LoginTextBox.Text))
+            {
+                LoginTextBox.BorderBrush = new SolidColorBrush(Colors.Red);
+                return;
+            }
+
+            if (!db.IsPasswordCorrectForUser(LoginTextBox.Text, PasswordBox.Password))
+            {
+                PasswordBox.BorderBrush = new SolidColorBrush(Colors.Red);
+                return;
+            }
+
+            PersonalArea personalArea = new(LoginTextBox.Text);
+            personalArea.Show();
+            Close();
+        }
+
+        private void FaceIDButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SignUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            Registration registration = new();
+            registration.Show();
+            Close();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void LoginTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            LoginTextBox.BorderBrush = new SolidColorBrush(Colors.Gray);
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordBox.BorderBrush = new SolidColorBrush(Colors.Gray);
+        }
+    }
+}
