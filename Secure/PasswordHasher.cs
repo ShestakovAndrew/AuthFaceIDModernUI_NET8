af401102d7e7ef585ec03581e88519fe8d5aab42
Андрей Шестаков <GlobalShow6@Gmail.com>
2023-05-16 17:45:11 +0300

    Поправил форматирование

--- Secure/PasswordHasher.cs
+++ Secure/PasswordHasher.cs
@@ -9,6 +9,25 @@ namespace AuthFaceIDModernUI.Secure
 {
     public static class PasswordHasher
     {
+        public static string Hash(string password)
+        {
+            byte[] salt;
+            byte[] buffer2;
+            if (password == null)
+            {
+                throw new ArgumentNullException("password");
+            }
+            using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, 0x10, 0x3e8))
+            {
+                salt = bytes.Salt;
+                buffer2 = bytes.GetBytes(0x20);
+            }
+            byte[] dst = new byte[0x31];
+            Buffer.BlockCopy(salt, 0, dst, 1, 0x10);
+            Buffer.BlockCopy(buffer2, 0, dst, 0x11, 0x20);
+            return Convert.ToBase64String(dst);
+        }
+
         public static bool Verify(string hashedPassword, string password)
         {
             byte[] buffer4;
@@ -34,25 +53,6 @@ namespace AuthFaceIDModernUI.Secure
                 buffer4 = bytes.GetBytes(0x20);
             }
             return ByteArraysEqual(buffer3, buffer4);
-        }
-
-        public static string Hash(string password)
-        {
-            byte[] salt;
-            byte[] buffer2;
-            if (password == null)
-            {
-                throw new ArgumentNullException("password");
-            }
-            using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, 0x10, 0x3e8))
-            {
-                salt = bytes.Salt;
-                buffer2 = bytes.GetBytes(0x20);
-            }
-            byte[] dst = new byte[0x31];
-            Buffer.BlockCopy(salt, 0, dst, 1, 0x10);
-            Buffer.BlockCopy(buffer2, 0, dst, 0x11, 0x20);
-            return Convert.ToBase64String(dst);
         }
 
         private static bool ByteArraysEqual(byte[] b1, byte[] b2)
