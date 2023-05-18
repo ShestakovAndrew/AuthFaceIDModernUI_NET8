1a526a298b62eafc70ba9f4d07ecccb2096b17ae
Андрей Шестаков <GlobalShow6@Gmail.com>
2023-05-18 11:43:27 +0300

    дополнил конфиг

--- Config.cs
+++ Config.cs
@@ -8,9 +8,17 @@
         public static string DataBasePath = System.IO.Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DataBaseName);
         public static string TempFolderPath = System.IO.Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), TempFolderName);
         public static string HaarCascadePath = System.IO.Path.Join(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\FaceID\\haarcascade_frontalface_default.xml");
+        public static string BaseDirectoryPath = System.IO.Path.Join(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..");
 
         public static string HostVisionAPI = "https://smarty.mail.ru/api/v1/persons/";
+        public static string HostASRAPI = "https://voice.mcs.mail.ru/asr";
+        public static string HostTTSAPI = "https://voice.mcs.mail.ru/tts";
+
         public static string OAuthProvider = "mcs";
-        public static string OAuthToken = "your token"; //set your token from VK Cloud
+
+        //set your tokens from VK Cloud
+        public static string OAuthTokenFaceID = "Токен для распознавания лиц";
+        public static string OAuthTokenSpeechSynthesis = "Токен для синтез речи";
+        public static string OAuthTokenAudioFileRecognition = "Токен для распознавания аудиофайлов";
     }
 }
