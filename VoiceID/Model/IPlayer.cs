bc1244c783af6ea88abe28db173d4d416e51863c
Андрей Шестаков <GlobalShow6@Gmail.com>
2023-05-18 22:46:18 +0300

    Обявил интерфейсы для записи и воспроизведения

---
+++ VoiceID/Model/IPlayer.cs
@@ -0,0 +1,23 @@
+﻿namespace AuthFaceIDModernUI.VoiceID.Model
+{
+    public class EventData : EventArgs { }
+
+    public enum EPlayStatus
+    {
+        Playing = 0,
+        Stopped,
+    };
+
+    public interface IPlayer
+    {
+        string FileName { set; get; }
+
+        event EventHandler<EventData> PlayingStoppedEvent;
+
+        void Play();
+
+        void Stop();
+        
+        bool IsActive();
+    }
+}
---
+++ VoiceID/Model/IRecorder.cs
@@ -0,0 +1,28 @@
+﻿namespace AuthFaceIDModernUI.VoiceID.Model
+{
+    public interface IDevice
+    {
+        string Name { get; set; }
+    }
+
+    public enum ERecordingStatus
+    {
+        Recording = 0,
+        Stopped,
+    };
+
+    public interface IRecorder
+    {
+        string OutFileName { set; get; }
+
+        List<IDevice> Devices { get; }
+        
+        IDevice ActiveDevice { get; set; }
+
+        void StartRecording();
+        
+        void StopRecording();
+        
+        bool IsActive();
+    }
+}
