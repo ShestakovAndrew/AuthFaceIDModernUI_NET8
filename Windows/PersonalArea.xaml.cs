a429f29883894654a10141dc4a7c5f0b5ba76953
Андрей Шестаков <GlobalShow6@Gmail.com>
2023-05-19 12:37:02 +0300

    Добавлен код воспроизведения сохранённого ранее аудио и конвертация его в текст для отображения

--- Windows/PersonalArea.xaml.cs
+++ Windows/PersonalArea.xaml.cs
@@ -1,7 +1,13 @@
 ﻿using AuthFaceIDModernUI.DataBase;
 using AuthFaceIDModernUI.FaceID;
 using AuthFaceIDModernUI.VoiceID;
+using AuthFaceIDModernUI.VoiceID.AudioEngine;
 using ModernLoginWindow;
+using NAudio.Wave;
+using System.Diagnostics;
+using System.IO;
+using System.Media;
+using System.Text;
 using System.Windows;
 using System.Windows.Input;
 
@@ -16,7 +22,8 @@ namespace AuthFaceIDModernUI.Windows
             InitializeComponent();
 
             TitleTextBlock.Text = (userLogin + TitleTextBlock.Text);
-            m_voiceFilePath = "";        }
+            m_voiceFilePath = string.Empty;        
+        }
 
         protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
         {
@@ -30,17 +37,42 @@ namespace AuthFaceIDModernUI.Windows
 
             if (recordVoice.ShowDialog() == true)
             {
+                m_voiceFilePath = recordVoice.m_outFilePath;
+                ListenRecordingButton.IsEnabled = true;
 
+                ConvertStereoToMonoWav(recordVoice);
+                SetTextFromFile();
+                VoiceExistBlock.Text = "Аудио распознано!";
             }
             else
             {
-
+                ListenRecordingButton.IsEnabled = false;
+                TextFromRecordTextBox.Clear();
             }
+        }
+
+        private void ConvertStereoToMonoWav(RecordVoice recordVoice)
+        {
+            var waveFileReader = new WaveFileReader(m_voiceFilePath);
+            var outFormat = new WaveFormat(waveFileReader.WaveFormat.SampleRate, 1);
+            var resampler = new MediaFoundationResampler(waveFileReader, outFormat);
+
+            recordVoice.UpdateFileName();
+            m_voiceFilePath = recordVoice.m_outFilePath;
+            WaveFileWriter.CreateWaveFile(m_voiceFilePath, resampler);
+        }
+
+        private async void SetTextFromFile()
+        {
+            TextFromRecordTextBox.Text = await VoiceTool.SpeechToText(m_voiceFilePath);
         }
 
         private void ListenRecordingButton_Click(object sender, RoutedEventArgs e)
         {
-
+            ListenRecordingButton.IsEnabled = false;
+            SoundPlayer player = new() { SoundLocation = m_voiceFilePath };
+            player.PlaySync();
+            ListenRecordingButton.IsEnabled = true;
         }
 
         private void ExitButton_Click(object sender, RoutedEventArgs e)
